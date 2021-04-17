b5d62837ec9ea8ff6b3e31493049825dff08a99e
Mahdi Aden <adenmahdi8@gmail.com>
2021-04-17 14:05:24 -0400

    Add files via upload

--- ASIFT.py
+++ ASIFT.py
@@ -1,105 +1,112 @@
 #!/usr/bin/python3
-import numpy as np
+import numpy
 import cv2
 from matplotlib import pyplot as plt
+from scipy.spatial import cKDTree
 import Equirec2Perspec as E2P
+import math
 
-def sift_alg(img1, img2, outImg, kp1, kp2):
+pan1 = cv2.imread('C:\\Users\\matty\\Downloads\\ImprovedASIFT-main\\ImprovedASIFT-main\\IMG8352.jpg')
+panumpyath1 = "C:\\Users\\matty\\Downloads\\ImprovedASIFT-main\\ImprovedASIFT-main\\IMG8352.jpg"
+
+pan2 = cv2.imread("C:\\Users\\matty\\Downloads\\ImprovedASIFT-main\\ImprovedASIFT-main\\IMG8354.jpg")
+panumpyath2 = "C:\\Users\\matty\\Downloads\\ImprovedASIFT-main\\ImprovedASIFT-main\\IMG8354.jpg"
+
+def sift_alg(img1, img2):
 	detector = cv2.SIFT_create()
 
 	kp1, des1 = detector.detectAndCompute(img1, None)
 	kp2, des2 = detector.detectAndCompute(img2, None)
-
-	bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+	if (len(kp1)==0 or len(kp2)==0):
+		print("No keypoints detected.")
+		return [],[],[]
+	bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
 
 	matches = bf.match(des1,des2)
 
 	matches = sorted(matches, key = lambda x:x.distance)
+	return matches, kp1, kp2
 
-	return matches
-
-def ASIFT(img1, img2, kp1, kp2):
+def ASIFT(img1, img2):
 	num_matches=0
-	for x in range(-5,5,0.1):
-		z=x/(2*numpy.pi)
-		for y in range(5,5,0.1):
-			w=y/(2*numpy.pi)
+	for x in range(-1,1,1):
+		z=x/(20*numpy.pi)
+		finalMatches = []
+		finalkp1 = []
+		finalkp2 = []
+		
 
-			A=[[numpy.cos(z),-numpy.sin(z),0],[numpy.sin(z), numpy.cos(z),0],[0,0,1]]
-			B=[[numpy.cos(w),-numpy.sin(w),0],[numpy.sin(w), numpy.cos(w),0],[0,0,1]]
-			img1_warp = cv.warpPerspective(img1, A, (img1.shape[1], img1.shape[0]))
-			img2_warp = cv.warpPerspective(img2, B, (img2.shape[1], img2.shape[0]))
+		A=numpy.array([[numpy.cos(z),-numpy.sin(z),0],[numpy.sin(z), numpy.cos(z),0],[0,0,1]]).astype(numpy.float32)
+		img1_warp = cv2.warpPerspective(img1, A, (img1.shape[1], img1.shape[0]))
 
-			matches=sift_alg(img1_warp, img2_warp, outImg, kp1, kp2)
-			if (len(matches)>num_matches):
-				final=matches
-				num_matches=len(matches)
+		matches, kp1, kp2=sift_alg(img1_warp, img2)
+		if (len(matches)>num_matches):
+			finalMatches=matches
+			finalkp1 = kp1
+			finalkp2 = kp2
+			num_matches=len(matches)
+	return finalMatches, finalkp1, finalkp2
 	
-	return matches
+#roughly translates points found on the warped images back to the panoramas
+def ImgPointsToPanoramaPoints(panaramaImg, imagePlace, kp):
 	
-#created my own function to find points
-def findPoints(image1, image2):
-    gray1 = cv2.cvtColor(np.copy(image1), cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(np.copy(image2), cv2.COLOR_BGR2GRAY)
+	increment =720/2#images are 720 by 1080 but increments are 50% in order for neighbors to share objects
+	zeroPlace = panaramaImg.shape[1]/2 - increment#first image center is panorama center
+	imageStartPoint = (imagePlace*increment+zeroPlace)%panaramaImg.shape[1]
+	minYshift = (panaramaImg.shape[0]-1080)/2 +math.floor(panaramaImg.shape[0]/20)
+	for i in range(len(kp)):
+		kp[i].pt=(kp[i].pt[0]+imageStartPoint, kp[i].pt[1]+minYshift)
+	return kp
 
 
-    orb = cv2.ORB_create()#create orb to us in matcher
-    keyPts1, desc1 = orb.detectAndCompute(gray1, None)#get key pts and descriptors 
-    keyPts2, desc2 = orb.detectAndCompute(gray2, None)
-
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)#initialize matcher
-    matches = bf.match(desc1, desc2)#match descriptors
-    sortedMatches = sorted(matches, key = lambda x:x.distance)#sort
-
-    srcPts  = np.float32([keyPts1[m.queryIdx].pt for m in sortedMatches]).reshape(-1,1,2)
-    dstPts  = np.float32([keyPts2[m.trainIdx].pt for m in sortedMatches]).reshape(-1,1,2)#return matched keypoints
-
-    M, mask = cv2.findHomography(srcPts, dstPts, cv2.RANSAC, 5.0)#get homorapghy matrix
-    h, w = image1.shape[:2]#hieght/width of img
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts, M)#perspective transform
+def SplitImage(img,path, min_size):
+    equ = E2P.Equirectangular(path)
+    imageWidth = 720
+    amountOfImages = math.floor(min_size/imageWidth)
+    splitImage= [None] * amountOfImages
+    xMaps=[None] * amountOfImages
+    yMaps=[None] * amountOfImages
     
-    h, w, x = image2.shape
-    
-    warpped = cv2.warpPerspective(image1, M, (w, h))#warp img to new perspective
+    increment = 360/amountOfImages
 
-    return warpped, dst
-
-#def mergeImages():
-#    leftRight, dst = findPoints(right,left)#gets matching points between right and left
-#    cv2.imshow("Warped",cv2.cvtColor((leftRight),cv2.COLOR_BGR2GRAY))#Shows warped in greyscale
-#    cv2.imshow("Merged",cv2.cvtColor((leftRight+left),cv2.COLOR_BGR2GRAY))#combine images in greyscale
-#    cv2.waitKey()
-#    cv2.destroyAllWindows()
-
-def image_call(x,y, img1, img2, img1_1, img2_1):
-    equ = E2P.Equirectangular(x)
-    
-    amountOfImages = 3
-    increment = 360/(amountOfImages+1)
-
-    for i in range(amountOfImages):
-        img1_1= equ.GetPerspective(60,i*increment, 0, img1.shape[1]/(amountOfImages-1), img1.shape[0])
-    
-    equ = E2P.Equirectangular(y)
+    cv2.waitKey(0)
     
     for i in range(amountOfImages):
-        img2_1= equ.GetPerspective(60,i*increment, 0, img2.shape[1]/(amountOfImages-1), img2.shape[0])
+        splitImage[i], xMaps[i], yMaps[i]= equ.GetPerspective(60,i*increment, 0, 720, 1080)
+        splitImage[i]= cv2.GaussianBlur(splitImage[i],(5,5),0)
+    
+    return equ, splitImage, xMaps, yMaps   		
+
+min_size = min(pan1.shape[1], pan2.shape[1])
 
 print("Image 1: ")
-x=input()
-print("Image 2: ")
-y=input()
-img1 = cv2.imread(x,0)
-img1_1=[]
-img2_1=[]
-image_call(x,y,img1, img2, img1_1,img2_1)
-total_matches=[]
+equ1, splitImage1, xMaps1, yMaps1 = SplitImage(pan1, panumpyath1, min_size)
+equ2, splitImage2, xMaps2, yMaps2 = SplitImage(pan2, panumpyath2, min_size)
+print("fin ")
 
-for x in img1_1:
-	num_matches=0
-	for y in img2_1:
-		matches=ASIFT(x,y, kp1, kp2)
-		if (len(matches)>num_matches):
-			final=matches
-			num_matches=len(matches)
+#matches, kp1, kp2=ASIFT(splitImage1[0],splitImage2[0])
+
+img3=255*numpy.ones(shape=[512, 512, 3], dtype=numpy.uint8)
+
+#kp1=ImgPointsToPanoramaPoints(pan1, 0,kp1)
+#kp2=ImgPointsToPanoramaPoints(pan2, 0,kp2)
+
+#this will work as long as the panorama's are the same size and the imagePlace variable are the same as the kp
+#img3=cv2.drawMatches(pan1,kp1,pan2,kp2,matches, img3)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#cv2.imwrite("C:\\Users\\matty\\Downloads\\ImprovedASIFT-main\\ImprovedASIFT-main\\Output.jpg", img3)
+
+y=min(len(splitImage1), len(splitImage2))
+
+for x in range(y):
+    matches, kp1, kp2=ASIFT(splitImage1[x],splitImage2[x])
+    if (len(matches)==0):
+        continue
+    else:
+        kp1=ImgPointsToPanoramaPoints(pan1, x,kp1)
+        kp2=ImgPointsToPanoramaPoints(pan2, x+1,kp2)
+        
+        img3=cv2.drawMatches(pan1,kp1,pan2,kp2,matches, img3)
+        cv2.imwrite("C:\\Users\\matty\\Downloads\\ImprovedASIFT-main\\ImprovedASIFT-main\\Output"+str(x)+".jpg", img3)
+        img3=255*numpy.ones(shape=[512, 512, 3], dtype=numpy.uint8)
